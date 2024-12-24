75c93a4e6a1f4b88f5592844d491289ec7d96a8d
gyur1kim <qri1114@gmail.com>
2024-12-24 10:57:12 +0900

    refactor: 혼자놀기의 달인 다시 풀기
    - 예전 코드보다 새로 작성한 게 가독성이 좋은 것 같다.

--- 혼자 놀기의 달인.js
+++ 혼자 놀기의 달인.js
@@ -1,29 +1,30 @@
 function solution(cards) {
-  function openBox(cardIdx, opendCards, opendCnt) {
-    let nextCardIdx = opendCards[cardIdx] - 1;
-    opendCnt += 1;
-    opendCards[cardIdx] = null;
-
-    if (opendCards[nextCardIdx] === null) {
-      return opendCnt;
-    }
-    return openBox(nextCardIdx, opendCards, opendCnt);
-  }
-
   let answer = 0;
 
-  for (let cardIdx = 0; cardIdx < cards.length; cardIdx++) {
-    let opendBoxes = [...cards];
-    let firstGroupResult = openBox(cardIdx, opendBoxes, 0);
+  function openBox(idx, visited, sum) {
+    const card = visited[idx];
 
-    for (let cardIdx2 = cardIdx + 1; cardIdx2 < cards.length; cardIdx2++) {
-      if (opendBoxes[cardIdx2]) {
-        let secondGroupResult = openBox(cardIdx2, opendBoxes, 0);
-        if (firstGroupResult * secondGroupResult > answer)
-          answer = firstGroupResult * secondGroupResult;
-      }
+    // 이미 열어본 박스면 끗
+    if (card === null) return sum;
+
+    // 안열어봤으면 다시 고고
+    visited[idx] = null;
+    return openBox(card - 1, visited, sum + 1);
+  }
+
+  for (let idx1 = 0; idx1 < cards.length; idx1++) {
+    const visited = [...cards];
+    const result1 = openBox(idx1, visited, 0);
+
+    for (let idx2 = idx1 + 1; idx2 < cards.length; idx2++) {
+      const result2 = openBox(idx2, visited, 0);
+
+      if (result1 * result2 > answer) answer = result1 * result2;
     }
   }
 
   return answer;
 }
+
+const input = [8, 6, 3, 7, 2, 5, 1, 4];
+console.log(solution(input));
