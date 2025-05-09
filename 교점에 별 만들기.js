62fe2dbdcb84bd4ab856ff900c1fa7842f44dc52
gyur1kim <qri1114@gmail.com>
2025-05-09 17:25:17 +0900

    feat: 교점에 별 만들기
    - 단순 구현 문제..!
    - y값은 큰 값에서 작은 값으로 구해야 하므로 maxY부터 --
    - x값은 작은 값부터 커지므로 for문에서 minX 부터 ++

---
+++ 교점에 별 만들기.js
@@ -0,0 +1,38 @@
+function solution(line) {
+  const intersection = [];
+
+  for (let i = 0; i < line.length; i++) {
+    for (let j = i + 1; j < line.length; j++) {
+      const line1 = line[i];
+      const line2 = line[j];
+
+      const [x, y] = getIntersection(...line1, ...line2);
+      if (Number.isInteger(x) && Number.isInteger(y)) intersection.push([x, y]);
+    }
+  }
+
+  const minX = Math.min(...intersection.map(([x, _]) => x));
+  const maxX = Math.max(...intersection.map(([x, _]) => x));
+  const minY = Math.min(...intersection.map(([_, y]) => y));
+  const maxY = Math.max(...intersection.map(([_, y]) => y));
+
+  const answer = [];
+
+  for (let i = maxY; i >= minY; i--) {
+    const res = [];
+    for (let j = minX; j <= maxX; j++) {
+      if (intersection.filter(([x, y]) => x === j && y === i).length) res.push("*");
+      else res.push(".");
+    }
+    answer.push(res.join(""));
+  }
+
+  return answer;
+
+  function getIntersection(a, b, e, c, d, f) {
+    const x = (b * f - e * d) / (a * d - b * c);
+    const y = (e * c - a * f) / (a * d - b * c);
+
+    return [x, y];
+  }
+}
